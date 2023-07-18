cfb3ef9db98fc51a098650e9fc8704b25484d326
TheEpicBlock <git.teb@theepicblock.nl>
2023-07-18 13:38:25 +0200

    Fix polymap not being NOP

--- src/main/java/nl/theepicblock/polymcshowcase/polymc/PolyMcHook.java
+++ src/main/java/nl/theepicblock/polymcshowcase/polymc/PolyMcHook.java
@@ -3,9 +3,11 @@ package nl.theepicblock.polymcshowcase.polymc;
 import io.github.theepicblock.polymc.api.PolyMap;
 import io.github.theepicblock.polymc.api.PolyMcEntrypoint;
 import io.github.theepicblock.polymc.api.PolyRegistry;
+import io.github.theepicblock.polymc.api.block.BlockPoly;
 import io.github.theepicblock.polymc.api.block.BlockStateManager;
 import io.github.theepicblock.polymc.api.block.BlockStateProfile;
 import io.github.theepicblock.polymc.api.misc.PolyMapProvider;
+import io.github.theepicblock.polymc.impl.NOPPolyMap;
 import io.github.theepicblock.polymc.impl.misc.BooleanContainer;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -22,7 +24,18 @@ public class PolyMcHook implements PolyMcEntrypoint {
     public static PolyMap NOP_POLY_MAP;
 
     static {
-        var registry = new PolyRegistry();
+        PolyRegistry registry = new PolyRegistry() {
+            @Override
+            public PolyMap build() {
+                var blocks = this.blockPolys;
+                return new NOPPolyMap() {
+                    @Override
+                    public BlockPoly getBlockPoly(Block block) {
+                        return blocks.get(block);
+                    }
+                };
+            }
+        };
         for (var profile : BlockStateProfile.ALL_PROFILES) {
             for (var block : profile.blocks) {
                 profile.onFirstRegister.accept(block, registry);
